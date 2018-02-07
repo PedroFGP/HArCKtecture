7a9164182750e416ae62d62d67cfcce15ada6cbf
Pedro Pereira <pedrofgp@outlook.com>
2018-02-07 14:58:52 -0200

    Começado desenvolvimento da interface dos desafios.

--- HArCKtecture/FrmMain.Designer.cs
+++ HArCKtecture/FrmMain.Designer.cs
@@ -28,9 +28,107 @@
         /// </summary>
         private void InitializeComponent()
         {
-            VisualPlus.Structure.Border border1 = new VisualPlus.Structure.Border();
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            VisualPlus.Structure.TextStyle textStyle1 = new VisualPlus.Structure.TextStyle();
+            VisualPlus.Structure.TextStyle textStyle2 = new VisualPlus.Structure.TextStyle();
+            VisualPlus.Structure.TextStyle textStyle3 = new VisualPlus.Structure.TextStyle();
+            VisualPlus.Structure.TextStyle textStyle4 = new VisualPlus.Structure.TextStyle();
+            VisualPlus.Structure.Border border1 = new VisualPlus.Structure.Border();
+            this.visualControlBox1 = new VisualPlus.Toolkit.Controls.Interactivity.VisualControlBox();
             this.SuspendLayout();
+            // 
+            // visualControlBox1
+            // 
+            this.visualControlBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.visualControlBox1.BackColor = System.Drawing.Color.Transparent;
+            // 
+            // 
+            // 
+            this.visualControlBox1.HelpButton.BackColorState.Disabled = System.Drawing.Color.Transparent;
+            this.visualControlBox1.HelpButton.BackColorState.Enabled = System.Drawing.Color.Transparent;
+            this.visualControlBox1.HelpButton.BackColorState.Hover = System.Drawing.Color.FromArgb(((int)(((byte)(190)))), ((int)(((byte)(186)))), ((int)(((byte)(186)))));
+            this.visualControlBox1.HelpButton.BackColorState.Pressed = System.Drawing.Color.FromArgb(((int)(((byte)(105)))), ((int)(((byte)(105)))), ((int)(((byte)(105)))));
+            this.visualControlBox1.HelpButton.BoxType = VisualPlus.Structure.ControlBoxButton.ControlBoxType.Default;
+            this.visualControlBox1.HelpButton.ForeColorState.Disabled = System.Drawing.Color.FromArgb(((int)(((byte)(105)))), ((int)(((byte)(105)))), ((int)(((byte)(105)))));
+            this.visualControlBox1.HelpButton.ForeColorState.Enabled = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.visualControlBox1.HelpButton.ForeColorState.Hover = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.visualControlBox1.HelpButton.ForeColorState.Pressed = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.visualControlBox1.HelpButton.Image = ((System.Drawing.Image)(resources.GetObject("resource.Image")));
+            this.visualControlBox1.HelpButton.Location = new System.Drawing.Point(0, 0);
+            this.visualControlBox1.HelpButton.MouseState = VisualPlus.Enumerators.MouseStates.Normal;
+            this.visualControlBox1.HelpButton.Name = "";
+            this.visualControlBox1.HelpButton.OffsetLocation = new System.Drawing.Point(0, 1);
+            this.visualControlBox1.HelpButton.Size = new System.Drawing.Size(24, 25);
+            this.visualControlBox1.HelpButton.TabIndex = 0;
+            this.visualControlBox1.HelpButton.Text = "s";
+            textStyle1.Disabled = System.Drawing.Color.Empty;
+            textStyle1.Enabled = System.Drawing.Color.Empty;
+            textStyle1.Hover = System.Drawing.Color.Empty;
+            textStyle1.TextRenderingHint = System.Drawing.Text.TextRenderingHint.ClearTypeGridFit;
+            this.visualControlBox1.HelpButton.TextStyle = textStyle1;
+            this.visualControlBox1.HelpButton.Visible = false;
+            this.visualControlBox1.Location = new System.Drawing.Point(560, 5);
+            // 
+            // 
+            // 
+            this.visualControlBox1.MaximizeButton.BackColorState.Disabled = System.Drawing.Color.Transparent;
+            this.visualControlBox1.MaximizeButton.BackColorState.Enabled = System.Drawing.Color.Transparent;
+            this.visualControlBox1.MaximizeButton.BackColorState.Hover = System.Drawing.Color.FromArgb(((int)(((byte)(190)))), ((int)(((byte)(186)))), ((int)(((byte)(186)))));
+            this.visualControlBox1.MaximizeButton.BackColorState.Pressed = System.Drawing.Color.FromArgb(((int)(((byte)(105)))), ((int)(((byte)(105)))), ((int)(((byte)(105)))));
+            this.visualControlBox1.MaximizeButton.BoxType = VisualPlus.Structure.ControlBoxButton.ControlBoxType.Default;
+            this.visualControlBox1.MaximizeButton.ForeColorState.Disabled = System.Drawing.Color.FromArgb(((int)(((byte)(105)))), ((int)(((byte)(105)))), ((int)(((byte)(105)))));
+            this.visualControlBox1.MaximizeButton.ForeColorState.Enabled = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.visualControlBox1.MaximizeButton.ForeColorState.Hover = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.visualControlBox1.MaximizeButton.ForeColorState.Pressed = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.visualControlBox1.MaximizeButton.Image = ((System.Drawing.Image)(resources.GetObject("resource.Image1")));
+            this.visualControlBox1.MaximizeButton.Location = new System.Drawing.Point(24, 0);
+            this.visualControlBox1.MaximizeButton.MouseState = VisualPlus.Enumerators.MouseStates.Normal;
+            this.visualControlBox1.MaximizeButton.Name = "";
+            this.visualControlBox1.MaximizeButton.OffsetLocation = new System.Drawing.Point(1, 1);
+            this.visualControlBox1.MaximizeButton.Size = new System.Drawing.Size(24, 25);
+            this.visualControlBox1.MaximizeButton.TabIndex = 2;
+            this.visualControlBox1.MaximizeButton.Text = "1";
+            textStyle2.Disabled = System.Drawing.Color.Empty;
+            textStyle2.Enabled = System.Drawing.Color.Empty;
+            textStyle2.Hover = System.Drawing.Color.Empty;
+            textStyle2.TextRenderingHint = System.Drawing.Text.TextRenderingHint.ClearTypeGridFit;
+            this.visualControlBox1.MaximizeButton.TextStyle = textStyle2;
+            // 
+            // 
+            // 
+            this.visualControlBox1.MinimizeButton.BackColorState.Disabled = System.Drawing.Color.Transparent;
+            this.visualControlBox1.MinimizeButton.BackColorState.Enabled = System.Drawing.Color.Transparent;
+            this.visualControlBox1.MinimizeButton.BackColorState.Hover = System.Drawing.Color.FromArgb(((int)(((byte)(190)))), ((int)(((byte)(186)))), ((int)(((byte)(186)))));
+            this.visualControlBox1.MinimizeButton.BackColorState.Pressed = System.Drawing.Color.FromArgb(((int)(((byte)(105)))), ((int)(((byte)(105)))), ((int)(((byte)(105)))));
+            this.visualControlBox1.MinimizeButton.BoxType = VisualPlus.Structure.ControlBoxButton.ControlBoxType.Default;
+            this.visualControlBox1.MinimizeButton.ForeColorState.Disabled = System.Drawing.Color.FromArgb(((int)(((byte)(105)))), ((int)(((byte)(105)))), ((int)(((byte)(105)))));
+            this.visualControlBox1.MinimizeButton.ForeColorState.Enabled = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.visualControlBox1.MinimizeButton.ForeColorState.Hover = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.visualControlBox1.MinimizeButton.ForeColorState.Pressed = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.visualControlBox1.MinimizeButton.Image = ((System.Drawing.Image)(resources.GetObject("resource.Image2")));
+            this.visualControlBox1.MinimizeButton.Location = new System.Drawing.Point(0, 0);
+            this.visualControlBox1.MinimizeButton.MouseState = VisualPlus.Enumerators.MouseStates.Normal;
+            this.visualControlBox1.MinimizeButton.Name = "";
+            this.visualControlBox1.MinimizeButton.OffsetLocation = new System.Drawing.Point(2, 0);
+            this.visualControlBox1.MinimizeButton.Size = new System.Drawing.Size(24, 25);
+            this.visualControlBox1.MinimizeButton.TabIndex = 1;
+            this.visualControlBox1.MinimizeButton.Text = "0";
+            textStyle3.Disabled = System.Drawing.Color.Empty;
+            textStyle3.Enabled = System.Drawing.Color.Empty;
+            textStyle3.Hover = System.Drawing.Color.Empty;
+            textStyle3.TextRenderingHint = System.Drawing.Text.TextRenderingHint.ClearTypeGridFit;
+            this.visualControlBox1.MinimizeButton.TextStyle = textStyle3;
+            this.visualControlBox1.MouseState = VisualPlus.Enumerators.MouseStates.Normal;
+            this.visualControlBox1.Name = "visualControlBox1";
+            this.visualControlBox1.Size = new System.Drawing.Size(72, 25);
+            this.visualControlBox1.TabIndex = 0;
+            this.visualControlBox1.Text = "visualControlBox1";
+            textStyle4.Disabled = System.Drawing.Color.Empty;
+            textStyle4.Enabled = System.Drawing.Color.Empty;
+            textStyle4.Hover = System.Drawing.Color.Empty;
+            textStyle4.TextRenderingHint = System.Drawing.Text.TextRenderingHint.ClearTypeGridFit;
+            this.visualControlBox1.TextStyle = textStyle4;
             // 
             // FrmMain
             // 
@@ -43,7 +141,9 @@
             this.Border.Thickness = 3;
             this.Border.Type = VisualPlus.Enumerators.ShapeType.Rectangle;
             this.Border.Visible = true;
-            this.ClientSize = new System.Drawing.Size(300, 300);
+            this.ClientSize = new System.Drawing.Size(640, 480);
+            this.Controls.Add(this.visualControlBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             border1.Color = System.Drawing.Color.FromArgb(((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))));
             border1.HoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(183)))), ((int)(((byte)(230)))));
             border1.HoverVisible = false;
@@ -52,7 +152,7 @@
             border1.Type = VisualPlus.Enumerators.ShapeType.Rounded;
             border1.Visible = false;
             this.Image.Border = border1;
-            this.Image.Image = ((System.Drawing.Bitmap)(resources.GetObject("resource.Image")));
+            this.Image.Image = ((System.Drawing.Bitmap)(resources.GetObject("resource.Image3")));
             this.Image.Point = new System.Drawing.Point(5, 7);
             this.Image.Size = new System.Drawing.Size(16, 16);
             this.Image.Visible = true;
@@ -63,6 +163,8 @@
         }
 
         #endregion
+
+        private VisualPlus.Toolkit.Controls.Interactivity.VisualControlBox visualControlBox1;
     }
 }
 
---
+++ HArCKtecture/User Controls/Challenge.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace HArCKtecture.User_Controls
+{
+    partial class Challenge
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            VisualPlus.Structure.TextStyle textStyle1 = new VisualPlus.Structure.TextStyle();
+            VisualPlus.Structure.TextStyle textStyle2 = new VisualPlus.Structure.TextStyle();
+            this.visualPanel1 = new VisualPlus.Toolkit.Controls.Layout.VisualPanel();
+            this.BtnPlay = new VisualPlus.Toolkit.Controls.Interactivity.VisualButton();
+            this.SuspendLayout();
+            // 
+            // visualPanel1
+            // 
+            this.visualPanel1.BackColorState.Disabled = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.visualPanel1.BackColorState.Enabled = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.visualPanel1.Border.Color = System.Drawing.Color.FromArgb(((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))));
+            this.visualPanel1.Border.HoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(183)))), ((int)(((byte)(230)))));
+            this.visualPanel1.Border.HoverVisible = true;
+            this.visualPanel1.Border.Rounding = 6;
+            this.visualPanel1.Border.Thickness = 1;
+            this.visualPanel1.Border.Type = VisualPlus.Enumerators.ShapeType.Rounded;
+            this.visualPanel1.Border.Visible = true;
+            this.visualPanel1.Font = new System.Drawing.Font("Segoe UI", 8.25F);
+            this.visualPanel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.visualPanel1.Location = new System.Drawing.Point(35, 20);
+            this.visualPanel1.MouseState = VisualPlus.Enumerators.MouseStates.Normal;
+            this.visualPanel1.Name = "visualPanel1";
+            this.visualPanel1.Padding = new System.Windows.Forms.Padding(5);
+            this.visualPanel1.Size = new System.Drawing.Size(180, 180);
+            this.visualPanel1.TabIndex = 0;
+            this.visualPanel1.Text = "visualPanel1";
+            textStyle1.Disabled = System.Drawing.Color.FromArgb(((int)(((byte)(131)))), ((int)(((byte)(129)))), ((int)(((byte)(129)))));
+            textStyle1.Enabled = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            textStyle1.Hover = System.Drawing.Color.Empty;
+            textStyle1.TextRenderingHint = System.Drawing.Text.TextRenderingHint.ClearTypeGridFit;
+            this.visualPanel1.TextStyle = textStyle1;
+            // 
+            // BtnPlay
+            // 
+            this.BtnPlay.BackColorState.Disabled = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.BtnPlay.BackColorState.Enabled = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.BtnPlay.BackColorState.Hover = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.BtnPlay.BackColorState.Pressed = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.BtnPlay.Border.Color = System.Drawing.Color.FromArgb(((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))));
+            this.BtnPlay.Border.HoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(183)))), ((int)(((byte)(230)))));
+            this.BtnPlay.Border.HoverVisible = true;
+            this.BtnPlay.Border.Rounding = 8;
+            this.BtnPlay.Border.Thickness = 1;
+            this.BtnPlay.Border.Type = VisualPlus.Enumerators.ShapeType.Rounded;
+            this.BtnPlay.Border.Visible = true;
+            this.BtnPlay.Font = new System.Drawing.Font("Segoe UI", 8.25F);
+            this.BtnPlay.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.BtnPlay.Image = null;
+            this.BtnPlay.Location = new System.Drawing.Point(77, 211);
+            this.BtnPlay.MouseState = VisualPlus.Enumerators.MouseStates.Normal;
+            this.BtnPlay.Name = "BtnPlay";
+            this.BtnPlay.Size = new System.Drawing.Size(98, 26);
+            this.BtnPlay.TabIndex = 1;
+            this.BtnPlay.Text = "Jogar";
+            this.BtnPlay.TextAlignment = System.Drawing.StringAlignment.Center;
+            this.BtnPlay.TextImageRelation = System.Windows.Forms.TextImageRelation.Overlay;
+            this.BtnPlay.TextLineAlignment = System.Drawing.StringAlignment.Center;
+            textStyle2.Disabled = System.Drawing.Color.FromArgb(((int)(((byte)(131)))), ((int)(((byte)(129)))), ((int)(((byte)(129)))));
+            textStyle2.Enabled = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            textStyle2.Hover = System.Drawing.Color.Empty;
+            textStyle2.TextRenderingHint = System.Drawing.Text.TextRenderingHint.ClearTypeGridFit;
+            this.BtnPlay.TextStyle = textStyle2;
+            // 
+            // Challenge
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Controls.Add(this.BtnPlay);
+            this.Controls.Add(this.visualPanel1);
+            this.Name = "Challenge";
+            this.Size = new System.Drawing.Size(248, 248);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private VisualPlus.Toolkit.Controls.Layout.VisualPanel visualPanel1;
+        private VisualPlus.Toolkit.Controls.Interactivity.VisualButton BtnPlay;
+    }
+}
