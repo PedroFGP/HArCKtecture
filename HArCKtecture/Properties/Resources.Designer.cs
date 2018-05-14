0181de5956c0a4276c677d58548b1453a57c46cf
Pedro Pereira <pedrofgp@outlook.com>
2018-05-14 14:45:39 -0300

    Consertado tutorial.

--- HArCKtecture/Properties/Resources.Designer.cs
+++ HArCKtecture/Properties/Resources.Designer.cs
@@ -113,6 +113,16 @@ namespace HArCKtecture.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Tutorial11 {
+            get {
+                object obj = ResourceManager.GetObject("Tutorial11", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Tutorial2 {
             get {
                 object obj = ResourceManager.GetObject("Tutorial2", resourceCulture);
